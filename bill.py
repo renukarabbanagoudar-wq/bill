c6230f3a32f31673a0d434df620f109a15f9e91a
Renuka <renuka.rabbanagoudar@gmail.com>
2025-12-04 17:44:37 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,11 +1,20 @@
+
 import sys
-if (sys.argv) ==2:
-    unit=sys.argv[1]
-    bill=sys.argv[2]
-    total=unit*bill
+
+RATE = 5
+
+if len(sys.argv) == 2:
+    print("User provided input values:")
+    script_name = sys.argv[0]
+    units = int(sys.argv[1])
 else:
-    print("Enter a electricity")
-    print("units:",units)
-    print("Bill:",bill)
+    print("No input given - using default values:")
+    script_name = sys.argv[0]
+    units = 100
 
+bill_amount = units * RATE
 
+print("Script Name:", script_name)
+print("Units Consumed:", units)
+print("Rate per Unit:", RATE)
+print("Total Electricity Bill:", bill_amount)
