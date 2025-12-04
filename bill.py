60403f72f423058e83edeb9804833cc569a7f9f3
Renuka <renuka.rabbanagoudar@gmail.com>
2025-12-04 17:06:28 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -2,9 +2,10 @@ import sys
 if (sys.argv) ==2:
     unit=sys.argv[1]
     bill=sys.argv[2]
-    total=unit * bill
+    total=unit*bill
 else:
     print("Enter a electricity")
-    print("Units:",units)
+    print("units:",units)
     print("Bill:",bill)
+
 
